61389ab8653f384c01c8d63608de2df7dc61f84d
vijaydevguy <vijayakumarjob01@gmail.com>
2025-07-12 18:31:08 +0530

    route strucuture

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ app.get('/',(req,res) => {
     res.json({msg: "Hello guys "});
 })
 
+//CRUD functionality of movies
+
+
+//R - for reading
+app.get('/movies', () =>{
+
+})
+
+//C - for create
+app.post('/movies', () =>{
+
+})
+
+//U - for update
+app.put('/movies/:id', () =>{
+
+})
+
+//D - for delete
+app.delete('/movies', () =>{
+
+})
+
 app.listen(6969, () =>{
     console.log(`This server is running at http://localhost:${PORT}`)
 })
