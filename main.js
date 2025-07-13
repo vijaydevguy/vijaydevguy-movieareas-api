85ec64fddb68cd454aa4e03e946f2353fc447b4d
vijaydevguy <vijayakumarjob01@gmail.com>
2025-07-13 19:07:34 +0530

    conneting mongodb with mongoose

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import express from "express"
 import movieRoutes from "./routes/movies.route.js";
+import connectDB from "./lib/db.js";
 
 const app = express();
 const PORT = 6969;
+
+//connect db
+connectDB();
+
 
 app.get('/',(req,res) => {
     console.log("first")
